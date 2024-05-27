753b33c3717766edb561ff9731c455548f38aa26
pranavdwivedi-184 <pranavdwivedi184@gmail.com>
2024-05-27 06:46:59 +0530

    jk

---
+++ Random.java
@@ -0,0 +1,8 @@
+public class Random {
+    public static void main(String[] args) {
+        System.out.println("\033[H\033[2J");
+
+        System.out.println(Math.round(Math.ceil(Math.random() * 6)));
+
+    }
+}
